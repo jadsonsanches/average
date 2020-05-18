de22ec3c2a8abe57d5415081e83d76668e214953
Jadson Sanches <jadson.sanches@gmail.com>
2020-05-18 10:16:43 -0300

    imgs

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Index from './src';
 
 export default function App() {
-  return (
-    <View>
-      <Text>AGORA VAI CARALHO!</Text>
-    </View>
-  );
+  return <Index />;
 }
