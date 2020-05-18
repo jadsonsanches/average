cb76bf19a115b8d886308ea1522ef848674ba0e2
Jadson Sanches <jadson.sanches@gmail.com>
2020-05-18 10:27:57 -0300

    FirebaseConfig

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBhufjx98916ys5ZKSs_Ckezvbsq4LsyvQ',
+  authDomain: 'average-f8a74.firebaseapp.com',
+  databaseURL: 'https://average-f8a74.firebaseio.com',
+  projectId: 'average-f8a74',
+  storageBucket: 'average-f8a74.appspot.com',
+  messagingSenderId: '823736060259',
+  appId: '1:823736060259:web:3d7f463560bad25af62b56',
+  measurementId: 'G-GMEDCVV8JW',
+};
